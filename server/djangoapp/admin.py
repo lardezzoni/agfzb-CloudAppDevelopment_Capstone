48f2dca1bfbcf071e6aec54017015c47d304cc82
Luiz <luizardezzoni2301@gmail.com>
2023-09-20 08:40:33 -0300

    Test10

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,4 +1,33 @@
 from django.contrib import admin
+# <HINT> Import any new Models here
+from .models import CarMake, CarModel
+
+# <HINT> Register QuestionInline and ChoiceInline classes here
+class CarModelInLine(admin.StackedInline):
+    model = CarModel
+    extra = 2
+
+class CarMakeInLine(admin.StackedInline):
+    model = CarMake
+    extra = 2
+
+
+# Register your models here.
+class CarMakeAdmin(admin.ModelAdmin):
+    inlines = [CarModelInLine]
+    list_display = ('name', 'description')
+    search_fields = ['name', 'description']
+
+
+class CarModelAdmin(admin.ModelAdmin):
+
+    list_display = ['car_model', "dealer_id", "name", 'type_model', 'year']
+    search_fields = ['car_model', "dealer_id", "name", 'type_model', 'year']
+
+
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
+
 # from .models import related models
 
 
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,5 +1,13 @@
-from django.db import models
+import sys
 from django.utils.timezone import now
+try:
+    from django.db import models
+except Exception:
+    print("There was an error loading django modules. Do you have django installed?")
+    sys.exit()
+
+from django.conf import settings
+import uuid
 
 
 # Create your models here.
@@ -9,6 +17,17 @@ from django.utils.timezone import now
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+class CarMake(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    name = models.CharField(max_length=50, default="default name")
+    description = models.CharField(max_length=200, default="default description")
+
+
+    def __str__(self):
+        return self.name
 
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
@@ -19,8 +38,17 @@ from django.utils.timezone import now
 # - Year (DateField)
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+class CarModel(models.Model):
 
+    car_model = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    dealer_id = models.IntegerField()
+    name = models.CharField(max_length=50, default="default model name")
+    type_model = models.CharField(max_length=50, default="default type model")
+    year = models.DateField(null=True)
 
+    def __str__(self):
+        return self.name
+    
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
 
 
